1f1665325b7b49d4d15a657800b00d6cc74da10f
GabeL-code <65963702+GabeL-code@users.noreply.github.com>
2021-01-14 11:17:11 -0600

    Add files via upload

---
+++ Resume_Portfolio/app.js
@@ -0,0 +1,42 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+     e.target.classList.add("active");
+
+       });
+
+
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btnn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btnn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
